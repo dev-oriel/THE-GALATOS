cf77aaca406a61b468cb6f8fb97acf21938ba3ac
dev-oriel <kiplangatoriel@gmail.com>
2026-02-26 23:51:55 +0300

    added routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
-import Navbar from "./components/Navbar";
-import AboutPage from "./pages/AboutPage";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Layout/Navbar";
+import Footer from "./components/Layout/Footer";
+import HomePage from "./pages/HomePage";
+import StoryPage from "./pages/StoryPage";
 
-function App() {
+export default function App() {
   return (
-    <div className="min-h-screen bg-background-light font-display">
-      <Navbar />
-      <AboutPage />
-      {/* You can add a Footer component here later */}
-    </div>
+    <Router>
+      <div className="flex min-h-screen flex-col selection:bg-primary/20">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<StoryPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
-
-export default App;
